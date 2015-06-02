fe77eeebaf6a14096456b1259e98fc6e0ecaea85
eyurtseven <yurt7emre@gmail.com>
2015-06-02 17:08:35 +0300

    panel görünümüne geçildi

--- ProductManagement.Admin/Scripts/site.js
+++ ProductManagement.Admin/Scripts/site.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function() {
     $('#tblCategoryList').dataTable();
     
-    $('#tblCategoryList').removeClass('display').addClass('table table-striped table-bordered'); 
+    $('#tblCategoryList').removeClass('display').addClass('table table-striped table-bordered');
+
+    $("#tblCategoryList_wrapper input[type=search]").css("width", "auto");
 });
